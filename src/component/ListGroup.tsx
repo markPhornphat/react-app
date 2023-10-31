4011d130ab3467f9c3679677c95f0ce67620353c
marklileo <phornphatmak1112@gmail.com>
2023-10-31 15:45:43 +0700

    Update ListGroup.tsx

--- src/component/ListGroup.tsx
+++ src/component/ListGroup.tsx
@@ -1,14 +1,12 @@
 import { MouseEvent, useState } from "react"; //useState ==> To tell the react that this variable is change over time
 
-interface Props {
-  //To change the list dynamically
+interface Props { //To change the list dynamically
   items: string[];
   heading: string;
   onSelectItem: (item: string) => void; //Send the function in and return void
 }
 
-function ListGroup({ items, heading, onSelectItem }: Props) {
-  //React application => "PascalCasing"
+function ListGroup({ items, heading, onSelectItem }: Props) { //React application => "PascalCasing"
   // items.map(item => <li> {item} </li> ) สั่งให้ item แต่ละไอ item เปลี่ยนเป็น format <li> {element in array} </li>
   //                      Note! React ต้องการให้เรา keep track list each item --> ใส่ key เข้าไป ในแต่ละ element (line: 14)
   const message = items.length === 0 ? <p>No item found!</p> : null;
@@ -38,7 +36,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
       <h1>{heading}</h1>
       {items.length === 0 ? <p>No item found!</p> : null}
       <ul className="list-group">
-        {items.map((item, index) => (
+        {items.map((item, index) => ( 
           <li
             className={
               selectedIndex === index
